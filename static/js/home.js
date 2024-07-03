6fa9019bf3fef673c2a137878549dead63e25f27
NerWars <soluxbot@gmail.com>
2024-07-03 15:03:16 -0600

    avanc_ de_ home

---
+++ static/js/home.js
@@ -0,0 +1,4 @@
+document.getElementById("logo-cuenta").addEventListener("click", function() {
+    const casita = document.querySelector("casita");
+    casita.style.left = "-100px"; // Mueve el cuadrado 100px hacia la derecha
+});
